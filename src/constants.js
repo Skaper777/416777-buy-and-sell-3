91097423d1a13dbcf5c2ee8261c1c81d8c480028
Skaper777 <skaper777@gmail.com>
2020-06-24 12:32:23 +0300

    Настройка сервера

--- src/constants.js
+++ src/constants.js
@@ -1,8 +1,11 @@
 'use strict';
 
 const DEFAULT_COMMAND = `--help`;
+const DEFAULT_PORT = 3000;
 const TARGET_ARGS_INDEX = 1;
 const USER_ARGV_INDEX = 2;
+
+const FILENAME = `mocks.json`;
 
 const OfferType = Object.freeze({
   OFFER: `offer`,
@@ -25,25 +28,42 @@ const СliMessage = Object.freeze({
   SUCCESS: `Operation success. File created.`
 });
 
+const ServerMessage = Object.freeze({
+  CREATE_ERROR: `Ошибка при создании сервера`,
+  PENDING: `Ожидаю соединения на `
+});
+
 const ExitCode = Object.freeze({
   SUCCESS: 0,
   ERROR: 1
 });
 
-const MockPath = {
+const MockPath = Object.freeze({
   SENTENCES_PATH: `./data/sentences.txt`,
   TITLES_PATH: `./data/titles.txt`,
   CATEGORIES_PATH: `./data/categories.txt`
-};
+});
+
+const HttpCode = Object.freeze({
+  OK: 200,
+  NOT_FOUND: 404,
+  INTERNAL_SERVER_ERROR: 500,
+  FORBIDDEN: 403,
+  UNAUTHORIZED: 401,
+});
 
 module.exports = {
   DEFAULT_COMMAND,
+  DEFAULT_PORT,
   TARGET_ARGS_INDEX,
   USER_ARGV_INDEX,
+  FILENAME,
   OfferType,
   SumRestrict,
   PictureRestrict,
   СliMessage,
+  ServerMessage,
   ExitCode,
-  MockPath
+  MockPath,
+  HttpCode
 };
