56d38f8e81d75532eb92bdf0ecef8ef0a8e78598
Skaper777 <skaper777@gmail.com>
2020-07-26 16:14:52 +0300

    small fixes

--- src/express/index.js
+++ src/express/index.js
@@ -5,6 +5,9 @@ const express = require(`express`);
 const mainRoutes = require(`./routes/main-routes`);
 const myRoutes = require(`./routes/my-routes`);
 const offersRoutes = require(`./routes/offers-routes`);
+const registerRoutes = require(`./routes/register-routes`);
+const loginRoutes = require(`./routes/login-routes`);
+const searchRoutes = require(`./routes/search-routes`);
 
 const {DEFAULT_PORT} = require(`../constants`);
 
@@ -12,6 +15,11 @@ const app = express();
 
 app.use(`/`, mainRoutes);
 app.use(`/my`, myRoutes);
+app.use(`/register`, registerRoutes);
+app.use(`/login`, loginRoutes);
+app.use(`/search`, searchRoutes);
 app.use(`/offers`, offersRoutes);
 
-app.listen(DEFAULT_PORT);
+app.listen(DEFAULT_PORT, () => {
+  console.info(`Сервер запущен на ${DEFAULT_PORT} порту`);
+});
