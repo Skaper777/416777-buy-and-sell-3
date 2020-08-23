7e5ef3743296aba8b50fe50f1191a1756c65d27e
Skaper777 <skaper777@gmail.com>
2020-08-23 13:57:05 +0300

    module4-task1

--- src/constants.js
+++ src/constants.js
@@ -2,12 +2,16 @@
 
 const DEFAULT_COMMAND = `--help`;
 const DEFAULT_PORT = 8080;
+const DEFAULT_COUNT = 1;
 const TARGET_ARGS_INDEX = 1;
 const USER_ARGV_INDEX = 2;
-
+const MAX_ADS = 1000;
+const MAX_ID_LENGTH = 5;
+const MAX_COMMENTS = 6;
 const FILENAME = `mocks.json`;
 const PUBLIC_DIR = `public`;
 const TEMPLATES_DIR = `templates`;
+const API_PREFIX = `/api`;
 
 const OfferType = Object.freeze({
   OFFER: `offer`,
@@ -43,25 +47,33 @@ const ExitCode = Object.freeze({
 const MockPath = Object.freeze({
   SENTENCES_PATH: `./data/sentences.txt`,
   TITLES_PATH: `./data/titles.txt`,
-  CATEGORIES_PATH: `./data/categories.txt`
+  CATEGORIES_PATH: `./data/categories.txt`,
+  COMMENTS_PATH: `./data/comments.txt`
 });
 
 const HttpCode = Object.freeze({
   OK: 200,
+  CREATED: 201,
   NOT_FOUND: 404,
   INTERNAL_SERVER_ERROR: 500,
   FORBIDDEN: 403,
   UNAUTHORIZED: 401,
+  BAD_REQUEST: 400,
 });
 
 module.exports = {
   DEFAULT_COMMAND,
   DEFAULT_PORT,
+  DEFAULT_COUNT,
   TARGET_ARGS_INDEX,
   USER_ARGV_INDEX,
+  MAX_ADS,
   FILENAME,
+  MAX_ID_LENGTH,
+  MAX_COMMENTS,
   PUBLIC_DIR,
   TEMPLATES_DIR,
+  API_PREFIX,
   OfferType,
   SumRestrict,
   PictureRestrict,
--- src/service/cli/commands/generate.js
+++ src/service/cli/commands/generate.js
@@ -1,8 +1,8 @@
 'use strict';
 
 const fs = require(`fs`).promises;
-
 const logger = require(`../../../logger`);
+const {nanoid} = require(`nanoid`);
 
 const {
   ExitCode,
@@ -10,7 +10,12 @@ const {
   OfferType,
   SumRestrict,
   PictureRestrict,
-  MockPath
+  MockPath,
+  DEFAULT_COUNT,
+  MAX_ADS,
+  MAX_ID_LENGTH,
+  MAX_COMMENTS,
+  FILENAME
 } = require(`../../../constants`);
 
 const {
@@ -20,18 +25,25 @@ const {
   readContent
 } = require(`../../../utils`);
 
-const DEFAULT_COUNT = 1;
-const MAX_ADS = 1000;
-const FILE_NAME = `mocks.json`;
+const generateComments = (count, comments) => {
+  return Array(count).fill({}).map(() => ({
+    id: nanoid(MAX_ID_LENGTH),
+    text: shuffle(comments)
+      .slice(0, getRandomInt(1, 3))
+      .join(` `)
+  }));
+};
 
-const generateOffers = (count, titles, categories, sentences) => (
+const generateOffers = (count, titles, categories, sentences, comments) => (
   Array(count).fill({}).map(() => ({
+    id: nanoid(MAX_ID_LENGTH),
     category: shuffle(categories).slice(0, getRandomInt(1, categories.length - 1)),
     description: shuffle(sentences).slice(1, 5).join(` `),
     picture: getPictureFileName(getRandomInt(PictureRestrict.MIN, PictureRestrict.MAX)),
     title: titles[getRandomInt(0, titles.length - 1)],
     type: Object.keys(OfferType)[Math.floor(Math.random() * Object.keys(OfferType).length)],
     sum: getRandomInt(SumRestrict.MIN, SumRestrict.MAX),
+    comments: generateComments(getRandomInt(1, MAX_COMMENTS), comments)
   }))
 );
 
@@ -42,10 +54,11 @@ module.exports = {
     const sentences = await readContent(MockPath.SENTENCES_PATH);
     const titles = await readContent(MockPath.TITLES_PATH);
     const categories = await readContent(MockPath.CATEGORIES_PATH);
+    const comments = await readContent(MockPath.COMMENTS_PATH);
 
     const [count] = args;
     const countOffer = Number.parseInt(count, 10) || DEFAULT_COUNT;
-    const content = JSON.stringify(generateOffers(countOffer, titles, categories, sentences));
+    const content = JSON.stringify(generateOffers(countOffer, titles, categories, sentences, comments));
 
     if (args > MAX_ADS) {
       logger.showError(СliMessage.LENGTH_ERROR);
@@ -53,7 +66,7 @@ module.exports = {
     }
 
     try {
-      await fs.writeFile(FILE_NAME, content);
+      await fs.writeFile(FILENAME, content);
       logger.showSuccess(СliMessage.SUCCESS);
       process.exit(ExitCode.SUCCESS);
     } catch (error) {
